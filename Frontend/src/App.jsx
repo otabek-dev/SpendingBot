427a7e1f8441ed6d3535b0bddda01b1f7049d10f
Otabek Rustamov <otabek.pro@hotmail.com>
2023-10-29 02:15:32 +0500

    Doughnut added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const expenseData = {
+    labels: ["Продукты", "Транспорт", "Жилье", "Развлечения", "Здоровье"],
+    datasets: [
+      {
+        data: [250, 150, 500, 100, 200], // Здесь укажите суммы расходов для каждой категории
+        backgroundColor: ["#FF5733", "#FFC300", "#33FF57", "#336CFF", "#B233FF"], // Цвета для каждой секции
+      },
+    ],
+  };
 
+  ChartJS.register(ArcElement, Tooltip, Legend);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <Doughnut data={expenseData} />
+    </div>
   )
 }
 
